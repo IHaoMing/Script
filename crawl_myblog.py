2b42f3804a858b4fbfa7b6f39686e9ca2b349031
IHaoMing <haoming.wu168@gmail.com>
2018-06-11 22:23:00 +0800

    craw my blog

--- crawl_myblog.py
+++ crawl_myblog.py
@@ -12,30 +12,39 @@ def getUrl():
     r1 =  requests.get(archives)
     s1 = BeautifulSoup(r1.text, 'html.parser', from_encoding='utf-8')
     for links in s1.find_all(class_="post-title-link"):
-        url.append('http://ihaoming.top'+links['href'])
-
+        t = links.span
+        u = 'http://ihaoming.top'+links['href']
+        d = {'title': t.string, 'url': u}
+        url.append(d)
+     
+        
     for i in range(2, 6):
         archives = "http://ihaoming.top/archives/page/" + str(i) + "/"
         r = requests.get(archives)
         s = BeautifulSoup(r.text, 'html.parser', from_encoding='utf-8')
         for links in s.find_all(class_="post-title-link"):
-            url.append('http://ihaoming.top'+links['href'])
+            t = links.span
+            u = 'http://ihaoming.top'+links['href']
+            d = {'title': t.string, 'url': u}
+            url.append(d)
     
     return url
 
 #获取所有文章html
 def getHtml(blog_url):
     blog = []
-    for url in blog_url:
-        r = requests.get(url)
+    for info in blog_url:
+        r = requests.get(info['url'])
         s = BeautifulSoup(r.text, 'html.parser')
         b = s.find(class_="post-body")
         lines = b.find_all(class_="gutter")
         [line.extract() for line in lines] 
-        b_str = b.content.decode('utf-8')
         h = ht.HTML2Text()
-        md = h.handle(b_str)
-        open("1.md","w").write(md) 
+        md = h.handle(str(b))
+        with open(info['title'] + ".md", "wb+") as f:
+            f.write(md.encode('utf-8'))
+        #open(str(i) + ".md","wb+").write(md.encode('utf-8')) 
+        f.close()
         blog.append(b)
     return blog
 
@@ -44,10 +53,15 @@ def saveAsMD(blog):
     pass
 
 def main():
-    u = "http://ihaoming.top/archives/1ca6b62d.html"
-    url =[]
-    url.append(u)
-    getHtml(url)
+    #u = {'title':'标题', 'url':'http://ihaoming.top/archives/643a1911.html'}
+    
+    #url =[]
+    #url.append(u)
+    #getHtml(url)
+    r = getUrl()
+    for info in r:
+        print(info['title'])
+        print(info['url'])
 
 if __name__ == '__main__':
     main()
